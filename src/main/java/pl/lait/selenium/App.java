5e2cc83c21508c09b9cda7bc8eda79db60672314
Adam Dzik <lasicmk2@gmail.com>
2019-06-09 12:35:48 +0200

    init commit

---
+++ src/main/java/pl/lait/selenium/App.java
@@ -0,0 +1,41 @@
+package pl.lait.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	static WebDriver driver;
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.setProperty("webdriver.chrome.driver", "/Users/adam/Documents/LAIT/chromedriver");
+    
+        driver = new ChromeDriver();
+        driver.get("http://newtours.demoaut.com");
+        
+        driver.findElement(By.linkText("CONTACT")).click();
+        sleep(2);
+        driver.findElement(By.linkText("SUPPORT")).click();
+        driver.findElement(By.linkText("REGISTER")).click();
+        driver.findElement(By.linkText("SIGN-ON")).click();
+        sleep(3);
+        driver.quit();
+    }
+    
+    public static void sleep(int seconds) {
+    	try {
+			Thread.sleep(seconds*1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+}
